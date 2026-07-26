3281813bf0c5d47ae570c810d6d8eae89959dde5
Kirizaki <krajekg@gmail.com>
2026-07-26 22:26:50 +0200

    [add]: test uploaded file

--- tests/test_upload.py
+++ tests/test_upload.py
@@ -15,3 +15,24 @@ def test_upload_returns_id(client):
     data = response.json()
     assert "id" in data
 
+
+def test_download_uploaded_file(client):
+    # arrange
+    test_content = "JESIOTR to smaczna ryba."
+    upload_response = client.post(
+        "/upload",
+        params={
+            "content": test_content
+        }
+    )
+    file_id = upload_response.json()["id"]
+
+    # act
+    response = client.get(
+        f"/download/{file_id}"
+    )
+
+    # assert
+    assert response.status_code == 200
+    assert response.json()["content"] == test_content
+
