3dbeb620c63bd3835f5600854cd58f46457812a4
Kirizaki <krajekg@gmail.com>
2026-07-26 23:09:32 +0200

    [add]: simple storage

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ from uuid import uuid4
 from fastapi import FastAPI
 from fastapi import HTTPException
 
-app = FastAPI()
+from app.storage import Storage
 
-files = {}
+app = FastAPI()
+storage = Storage()
 
 
 @app.get("/health")
@@ -16,27 +17,31 @@ def health():
 def upload(content: str):
     file_id = str(uuid4())
 
-    files[file_id] = {
-        "content": content
-    }
+    storage.save(file_id, content)
 
     return { "id": file_id }
 
 
+def raise_file_not_found():
+    raise HTTPException(status_code=404, detail="File not found")
+
+
 @app.get("/download/{file_id}")
 def download(file_id: str):
-    if file_id not in files:
-        raise HTTPException(status_code=404, detail="File not found")
+    file = storage.get(file_id)
+    if file is None:
+        raise_file_not_found()
     
-    return files[file_id]
+    return { "content": file}
 
 
 @app.delete("/file/{file_id}")
 def delete(file_id: str):
-    if file_id not in files:
-        raise HTTPException(status_code=404, detail="File not found")
+    file = storage.get(file_id)
+    if file is None:
+        raise_file_not_found()
     
-    del files[file_id]
+    storage.delete(file_id)
 
     return {"deleted": True}
 
---
+++ app/storage.py
@@ -0,0 +1,13 @@
+class Storage:
+    def __init__(self):
+        self.files = {}
+
+    def save(self, file_id, content):
+        self.files[file_id] = content
+
+    def get(self, file_id):
+        return self.files.get(file_id, None)
+
+    def delete(self, file_id):
+        del self.files[file_id]
+
