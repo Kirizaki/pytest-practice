252ed6b978534db89f915b0e583968b3efd13938
Kirizaki <krajekg@gmail.com>
2026-07-26 22:36:26 +0200

    [update]: remove redundand negative download test

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -12,12 +12,8 @@ def test_delete_uploaded_file(client):
     delete_response = client.delete(
         f"/file/{file_id}"
     )
-    download_response = client.get(
-        f"/download/{file_id}"
-    )
 
     # assert
     assert delete_response.status_code == 200
     assert delete_response.json()["deleted"] is True
-    assert download_response.status_code == 404
 
