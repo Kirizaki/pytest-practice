e1587e964c7a8177ec4aa7d13fd825e1bcf9ac82
Kirizaki <krajekg@gmail.com>
2026-07-26 22:40:14 +0200

    [update]: actually seperate negative test

--- tests/test_delete.py
+++ tests/test_delete.py
@@ -17,3 +17,25 @@ def test_delete_uploaded_file(client):
     assert delete_response.status_code == 200
     assert delete_response.json()["deleted"] is True
 
+
+def test_deleted_file_cannot_be_downloaded(client):
+    # arrange
+    upload_response = client.post(
+        "/upload",
+        params={
+            "content": "not for long..."
+        }
+    )
+    file_id = upload_response.json()["id"]
+    client.delete(
+        f"/file/{file_id}"
+    )
+
+    # act
+    response = client.get(
+        f"/download/{file_id}"
+    )
+
+    # assert
+    assert response.status_code == 404
+
