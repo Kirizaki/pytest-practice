d6727cadd3d398533ee72d5fbfd215d536803cab
Kirizaki <krajekg@gmail.com>
2026-07-26 21:42:06 +0200

    [add]: upload endpoint

--- app/main.py
+++ app/main.py
@@ -1,9 +1,23 @@
+from uuid import uuid4
+
 from fastapi import FastAPI
 
 app = FastAPI()
+
+files = {}
 
 
 @app.get("/health")
 def health():
     return {"status": "ok"}
+
+@app.post("/upload")
+def upload(content: str):
+    file_id = str(uuid4())
+
+    files[file_id] = {
+        "content": content
+    }
+
+    return { "id": file_id }
 
